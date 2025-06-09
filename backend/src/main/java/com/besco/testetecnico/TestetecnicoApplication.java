2a713c47d93f6254d077e23e4ce219873481cd26
Arthur Kramberger <arthur1curi@gmail.com>
2025-06-09 19:25:51 -0300

    Spring Initialr

---
+++ backend/src/main/java/com/besco/testetecnico/TestetecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.besco.testetecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestetecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestetecnicoApplication.class, args);
+	}
+
+}
